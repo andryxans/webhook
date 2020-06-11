fa8d6d7f82340eef739d99de126c3f2234fc0c15
a.albert <Fkm,thn944>
2020-06-11 16:29:39 +0700

    bot first webHuck

--- bot.py
+++ bot.py
@@ -1,5 +1,36 @@
-import telebot
-import config # подключаем конфиг, чтобы взять с него токен бота
+import os
 
-bot = telebot.TeleBot(config.TOKEN)
-print(bot.get_me())
+from flask import Flask, request
+
+import telebot
+
+TOKEN = '444966761:AAGNCSYImN_O3v-A2bkkSZc_bhIW-hBJ7FA'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://stormy-peak-70033.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
