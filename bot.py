07e5dbde999683d24286a02095aaeb838a555336
a.albert <Fkm,thn944>
2020-06-11 17:27:44 +0700

    bot first webHuck

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(message.chat_id, message.text)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
