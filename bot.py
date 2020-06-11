f851992b31d21a0ba2bc0d74eb0dfa9916b6851b
a.albert <Fkm,thn944>
2020-06-11 15:19:04 +0700

    bot first webHuck

--- bot.py
+++ bot.py
@@ -33,4 +33,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="https://stormy-peak-70033.herokuapp.com/", port=int(os.environ.get('PORT', 5000)))
