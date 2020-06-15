ea21d95da8452886bc78be2408759485cd4e3bb9
a.albert <Fkm,thn944>
2020-06-15 13:21:55 +0700

    Bot WH game

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-TOKEN = '444966761:AAGNCSYImN_O3v-A2bkkSZc_bhIW-hBJ7FA'  # Токен бота
+TOKEN = '***'  # Токен бота
 APP_NAME = 'stormy-peak-70033'
 database_name = 'music.db'  # Файл с базой данных
 shelve_name = 'shelve.db'  # Файл с хранилище
