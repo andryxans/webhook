fbc5c5293a36b34b8d3455e848f2c91444b621ff
a.albert <Fkm,thn944>
2020-06-15 11:04:10 +0700

    First game WH

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
-TOKEN = '***'  # Токен бота
+TOKEN = '444966761:AAGNCSYImN_O3v-A2bkkSZc_bhIW-hBJ7FA'  # Токен бота
 APP_NAME = 'stormy-peak-70033'
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилище
